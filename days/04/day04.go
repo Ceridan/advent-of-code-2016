bb3596da08ee8fa12b41773125fb7128ea25d0c6
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2021-01-04 17:16:59 +0300

    feat: Solution for day04 parts 1.

---
+++ days/04/day04.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type room struct {
+	names    []string
+	sectorId int
+	checksum string
+}
+
+func Part1(rooms []room) int {
+	valid := 0
+
+	for _, r := range rooms {
+		checksum := calculateChecksum(r.names)
+		if checksum == r.checksum {
+			valid += r.sectorId
+		}
+	}
+
+	return valid
+}
+
+func Part2(rooms []room) int {
+	return 0
+}
+
+func calculateChecksum(names []string) string {
+	nameToCount := make(map[rune]int)
+	for _, w := range names {
+		for _, r := range []rune(w) {
+			_, ok := nameToCount[r]
+			if ok {
+				nameToCount[r] += 1
+			} else {
+				nameToCount[r] = 1
+			}
+		}
+	}
+
+	countToName := make(map[int][]string)
+	countSet := make(map[int]bool)
+	for k, v := range nameToCount {
+		_, ok := countToName[v]
+		if ok {
+			countToName[v] = append(countToName[v], string(k))
+		} else {
+			countToName[v] = []string{string(k)}
+		}
+		countSet[v] = true
+	}
+
+	counts := make([]int, 0, len(countSet))
+	for k := range countSet {
+		counts = append(counts, k)
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(counts)))
+
+	checksum := make([]string, 0, 5)
+	for _, v := range counts {
+		sort.Strings(countToName[v])
+		for _, ch := range countToName[v] {
+			checksum = append(checksum, ch)
+
+			if len(checksum) == 5 {
+				return strings.Join(checksum, "")
+			}
+		}
+	}
+	return ""
+}
+
+func parseInput(input string) []room {
+	var rooms []room
+	re := regexp.MustCompile(`([a-z]+)|(\d)+`)
+
+	for _, line := range strings.Split(input, "\n") {
+		if line == "" {
+			continue
+		}
+
+		parsed := re.FindAll([]byte(line), -1)
+		names := make([]string, len(parsed))
+		for i, w := range parsed {
+			names[i] = string(w)
+		}
+
+		sectorId, _ := strconv.Atoi(names[len(parsed)-2])
+
+		r := room{
+			names:    names[:len(names)-2],
+			sectorId: sectorId,
+			checksum: names[len(names)-1],
+		}
+		rooms = append(rooms, r)
+	}
+	return rooms
+}
+
+func main() {
+	input, err := ioutil.ReadFile("days/04/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	rooms := parseInput(string(input))
+
+	fmt.Printf("Day 04, part 1: %v\n", Part1(rooms))
+	fmt.Printf("Day 04, part 2: %v\n", Part2(rooms))
+}
---
+++ days/04/day04_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCalculateChecksum(t *testing.T) {
+	fixtures := []struct {
+		input    []string
+		expected string
+	}{
+		{
+			input:    []string{"aaaaa", "bbb", "z", "y", "x"},
+			expected: "abxyz",
+		},
+		{
+			input:    []string{"aaaaa", "bbbbbbbb", "z", "y", "x"},
+			expected: "baxyz",
+		},
+		{
+			input:    []string{"not", "a", "real", "room"},
+			expected: "oarel",
+		},
+	}
+
+	for _, fxt := range fixtures {
+		in := strings.Join(fxt.input, "-")
+		t.Run(in, func(t *testing.T) {
+			got := calculateChecksum(fxt.input)
+			if got != fxt.expected {
+				t.Errorf("Day 01, calculateChecksum([%s]) = %s; want: %s", in, got, fxt.expected)
+			}
+		})
+	}
+}
+
+func TestPart1(t *testing.T) {
+	input := `
+aaaaa-bbb-z-y-x-123[abxyz]
+a-b-c-d-e-f-g-h-987[abcde]
+a-b-c-d-e-f-g-h-987[bcdef]
+not-a-real-room-404[oarel]
+totally-real-room-200[decoy]
+`
+	expected := 1514
+
+	rooms := parseInput(input)
+	got := Part1(rooms)
+
+	if got != expected {
+		t.Errorf("Day 04, part1 = %d; want: %d", got, expected)
+	}
+}
