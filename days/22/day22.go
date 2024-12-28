bbaab1306b35559851fee83ef6eccf13edf2b731
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2024-12-28 22:29:14 +0100

    feat: Day 22, part 2.

--- days/22/day22.go
+++ days/22/day22.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"sort"
 	"strconv"
 	"strings"
 )
+
+var directions = []*Point{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
 
 type Point struct {
 	x int
@@ -20,30 +23,95 @@ type Node struct {
 	available int
 }
 
+func (p *Point) add(other *Point) *Point {
+	return &Point{x: p.x + other.x, y: p.y + other.y}
+}
+
+func (p *Point) equals(other *Point) bool {
+	return p.x == other.x && p.y == other.y
+}
+
 func parseInput(data []string) []Node {
 	nodes := make([]Node, len(data)-2)
-	r := regexp.MustCompile("/dev/grid/node-x(\\d+)-y(\\d+)")
+	r := regexp.MustCompile("(\\d+)")
 	for i := 2; i < len(data); i++ {
-		s := strings.ReplaceAll(data[i], " ", "[]")
-		s = strings.ReplaceAll(s, "][", "")
-		s = strings.ReplaceAll(s, "[]", " ")
-		split := strings.Split(s, " ")
-		matches := r.FindAllStringSubmatch(split[0], -1)
+		matches := r.FindAllStringSubmatch(data[i], -1)
 		x, _ := strconv.Atoi(matches[0][1])
-		y, _ := strconv.Atoi(matches[0][2])
-		point := &Point{x, y}
-		size, _ := strconv.Atoi(split[1][:len(split[1])-1])
-		used, _ := strconv.Atoi(split[2][:len(split[2])-1])
-		available, _ := strconv.Atoi(split[3][:len(split[3])-1])
-		nodes[i-2] = Node{point, size, used, available}
+		y, _ := strconv.Atoi(matches[1][1])
+		size, _ := strconv.Atoi(matches[2][1])
+		used, _ := strconv.Atoi(matches[3][1])
+		available, _ := strconv.Atoi(matches[4][1])
+		nodes[i-2] = Node{&Point{x, y}, size, used, available}
 	}
+
+	sort.Slice(nodes, func(i, j int) bool {
+		if nodes[i].point.y == nodes[j].point.y {
+			return nodes[i].point.x < nodes[j].point.x
+		}
+		return nodes[i].point.y < nodes[j].point.y
+	})
+
 	return nodes
+}
+
+func buildGrid(nodes []Node) [][]Node {
+	hx, hy := nodes[len(nodes)-1].point.x, nodes[len(nodes)-1].point.y
+	grid := make([][]Node, hy+1)
+	for y := 0; y < hy+1; y++ {
+		grid[y] = make([]Node, hx+1)
+		for x := 0; x < hx+1; x++ {
+			grid[y][x] = nodes[y*(hx+1)+x]
+		}
+	}
+	return grid
+}
+
+func bfs(grid [][]Node, startNode *Node, targetNode *Node) int {
+	queue := []struct {
+		pos   *Point
+		steps int
+	}{{pos: startNode.point, steps: 0}}
+	visited := make(map[Point]bool)
+
+	for len(queue) > 0 {
+		item := queue[0]
+		queue = queue[1:]
+		if item.pos.equals(targetNode.point) {
+			return item.steps
+		}
+		if item.pos.y < 0 || item.pos.y >= len(grid) || item.pos.x < 0 || item.pos.x >= len(grid[0]) {
+			continue
+		}
+		if grid[item.pos.y][item.pos.x].used > 2*startNode.size {
+			continue
+		}
+		if _, ok := visited[*item.pos]; ok {
+			continue
+		}
+		visited[*item.pos] = true
+
+		for _, dir := range directions {
+			queue = append(queue, struct {
+				pos   *Point
+				steps int
+			}{pos: item.pos.add(dir), steps: item.steps + 1})
+		}
+	}
+	return -1
+}
+
+func findEmptyNode(nodes []Node) *Node {
+	for _, node := range nodes {
+		if node.used == 0 {
+			return &node
+		}
+	}
+	return nil
 }
 
 func Part1(data []string) int {
 	nodes := parseInput(data)
 	viable := 0
-
 	for i := 0; i < len(nodes)-1; i++ {
 		for j := i + 1; j < len(nodes); j++ {
 			if nodes[i].used > 0 && nodes[i].used <= nodes[j].available {
@@ -58,7 +126,22 @@ func Part1(data []string) int {
 }
 
 func Part2(data []string) int {
-	return 0
+	nodes := parseInput(data)
+	emptyNode := findEmptyNode(nodes)
+	if emptyNode == nil {
+		fmt.Println("Empty node not found")
+		os.Exit(1)
+	}
+	grid := buildGrid(nodes)
+	leftFromTargetNode := grid[0][len(grid[0])-2]
+
+	steps := bfs(grid, emptyNode, &leftFromTargetNode)
+	if steps == -1 {
+		fmt.Println("Impossible to move data from target node")
+		os.Exit(1)
+	}
+	steps = steps + leftFromTargetNode.point.x*5 + 1
+	return steps
 }
 
 func main() {
--- days/22/day22_test.go
+++ days/22/day22_test.go
@@ -4,13 +4,25 @@ import (
 	"testing"
 )
 
-func TestPart1(t *testing.T) {
-	expected := 0
-	var data []string
+func TestPart2(t *testing.T) {
+	expected := 7
+	data := []string{
+		"root@ebhq-gridcenter# df -h",
+		"Filesystem            Size  Used  Avail  Use%",
+		"/dev/grid/node-x0-y0   10T    8T     2T   80%",
+		"/dev/grid/node-x0-y1   11T    6T     5T   54%",
+		"/dev/grid/node-x0-y2   32T   28T     4T   87%",
+		"/dev/grid/node-x1-y0    9T    7T     2T   77%",
+		"/dev/grid/node-x1-y1    8T    0T     8T    0%",
+		"/dev/grid/node-x1-y2   11T    7T     4T   63%",
+		"/dev/grid/node-x2-y0   10T    6T     4T   60%",
+		"/dev/grid/node-x2-y1    9T    8T     1T   88%",
+		"/dev/grid/node-x2-y2    9T    6T     3T   66%",
+	}
 
-	got := Part1(data)
+	got := Part2(data)
 
 	if got != expected {
-		t.Errorf("Day 22, part1 = %d; want: %d", got, expected)
+		t.Errorf("Day 22, part2 = %d; want: %d", got, expected)
 	}
 }
